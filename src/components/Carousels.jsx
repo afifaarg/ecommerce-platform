023f26f441eaf6e9cc74d1d02941ea28af7d2a90
afifaarg <argoubafifa1@gmail.com>
2024-10-29 20:30:18 +0100

    Add signInSignup + slides

--- src/components/Carousels.jsx
+++ src/components/Carousels.jsx
@@ -1,197 +1,74 @@
-import { useEffect, useState, useRef } from "react";
-import { useSwipeable } from "react-swipeable";
+import React, { useState, useEffect } from "react";
+export default function Carousel() {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-function Carousel() {
-  const [slides, setSlides] = useState([
-    {
-      id: 1,
-      src: "https://pbs.twimg.com/media/EG75BrQU4AAx7p9.jpg",
-      text: "Confort ultime à prix réduit ! Profitez de nos matelas haut de gamme avec des remises exclusives. Transformez vos nuits en moments de pure détente.",
-    },
-    {
-      id: 2,
-      src: "https://www.slumbersearch.com/img/banner-header.jpg",
-      text: "Réinvente ton intérieur avec nos meubles tendances ! Design moderne et qualité supérieure, à des prix imbattables. Offre spéciale cette semaine !",
-    },
-    {
-      id: 3,
-      src: "https://pbs.twimg.com/media/EG75BrQU4AAx7p9.jpg",
-      text: "Apportez une touche d'élégance à votre maison avec nos objets de décoration. Promotions exceptionnelles sur une large gamme d'articles !",
-    },
-  ]);
-  const [index, setIndex] = useState(0);
-  const intervalRef = useRef(null);
+  // Array of image paths
+  const images = [
+    "https://pictureserver.net/pic_storage/pic/a0/73/french_src_mobilebanner_picid_8896_image.jpg?ver=1", // Replace with actual image paths
+    "https://contract.sonpura.com/WebRoot/StoreContract/Shops/SonpuraContract/MediaGallery/landing_colchones/Banner_landing_FR_comprimido.jpg",
+    "https://www.lematelas.fr/media//wysiwyg/page_cms/page_promotion/2023-05-epeda-721x250pagecms-min.jpg",
+  ];
 
+  // Automatically switch to the next slide every 5 seconds
   useEffect(() => {
-    startAutoSlide();
-    return () => {
-      clearInterval(intervalRef.current);
-    };
-  }, []);
+    const slideInterval = setInterval(() => {
+      nextSlide();
+    }, 5000); // Change slide every 5 seconds
 
-  const startAutoSlide = () => {
-    clearInterval(intervalRef.current);
-    intervalRef.current = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % slides.length);
-    }, 2000);
-  };
+    return () => clearInterval(slideInterval); // Cleanup interval on component unmount
+  }, [currentSlide]);
 
+  // Function to go to the next slide
   const nextSlide = () => {
-    setIndex((prevIndex) => (prevIndex + 1) % slides.length);
-    startAutoSlide();
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
   };
 
+  // Function to go to the previous slide
   const prevSlide = () => {
-    setIndex((prevIndex) =>
-      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + images.length) % images.length
     );
-    startAutoSlide();
   };
-
-  if (!slides.length || !slides[index]) {
-    return null;
-  }
-
   return (
-    <section className="relative ">
-      {/* Carousel Section */}
-      <div className="relative py-36 flex content-center items-center justify-center min-h-screen-75">
-        <div
-          className="absolute top-0 w-full h-full bg-center bg-cover"
-          style={{
-            backgroundImage: `url(${slides[index].src})`,
-          }}
-        >
-          <span
-            id="blackOverlay"
-            className="w-full h-full absolute opacity-75 bg-black"
-          ></span>
-        </div>
-        <div className="container relative mx-auto">
-          <div className="items-center flex flex-wrap">
-            <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-              <div className="text-center">
-                <h1 className="text-secondary font-semibold text-2xl mb-4">
-                  {slides[index].text}
-                </h1>
-                <p className="text-secondary-dark text-center text-xl">
-                  Profitez de nos meilleures offres maintenant !
-                </p>
-              </div>
-            </div>
+    <>
+      {/* Carousel */}
+      <div className="relative overflow-hidden">
+        {/* Carousel */}
+        <div className="relative overflow-hidden  flex content-center items-center justify-center h-[500px]">
+          {/* Wrapper for sliding animation */}
+          <div
+            className="flex transition-transform duration-1000 ease-in-out h-full h-[500px]" // Adjust the duration to 1000ms (1 second) for slower transitions
+            style={{
+              transform: `translateX(-${currentSlide * 100}%)`,
+              width: `${images.length * 100}%`,
+            }}
+          >
+            {/* Carousel Images using img tag for better control */}
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Slide ${index}`}
+                className=" object-cover flex-shrink-0 w-full h-full bg-center bg-cover" // object-cover ensures the image fills the container while maintaining aspect ratio
+              />
+            ))}
           </div>
-        </div>
 
-        {/* Navigation Arrows */}
-        <div className="absolute inset-y-0 left-0 flex items-center justify-center">
-          <button onClick={prevSlide} className="text-white text-4xl p-4">
+          {/* Carousel Navigation Buttons */}
+          <span
+            onClick={prevSlide}
+            className="absolute cursor-default left-0 top-1/2 transform -translate-y-1/2 bg-white text-primary font-bold py-2 px-4 opacity-50 hover:opacity-100"
+          >
             &#10094;
-          </button>
-        </div>
-        <div className="absolute inset-y-0 right-0 flex items-center justify-center">
-          <button onClick={nextSlide} className="text-white text-4xl p-4">
+          </span>
+          <span
+            onClick={nextSlide}
+            className="absolute cursor-default right-0 top-1/2 transform -translate-y-1/2 bg-white text-primary font-bold py-2 px-4 opacity-50 hover:opacity-100"
+          >
             &#10095;
-          </button>
-        </div>
-
-        {/* Pagination Dots */}
-        <div className="absolute bottom-0 w-full flex justify-center">
-          {slides.map((_, idx) => (
-            <span
-              key={idx}
-              className={`h-3 w-3 rounded-full mx-2 ${
-                idx === index ? "bg-white" : "bg-gray-500"
-              }`}
-            ></span>
-          ))}
+          </span>
         </div>
       </div>
-
-      {/* Cards Section */}
-      <section className="pb-12  -mt-20">
-        <div className="container mx-auto px-4">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <div className="px-4 text-center">
-              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                <div className="px-4 py-5 flex-auto">
-                  <div className="text-white bg-primary p-3 text-center inline-flex items-center justify-center  mb-5 shadow-lg rounded-full">
-                    <svg
-                      viewBox="0 0 16 16"
-                      fill="currentColor"
-                      height="2em"
-                      width="2em"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M15 9.5L10.5 14 9 12.5l-1 1 2.5 2.5 5.5-5.5z"
-                      />
-                      <path
-                        fill="currentColor"
-                        d="M7 12h5v-1.799c-1.05-.613-2.442-1.033-4-1.16v-.825c1.102-.621 2-2.168 2-3.716C10 2.015 10 0 7 0S4 2.015 4 4.5c0 1.548.898 3.095 2 3.716v.825C2.608 9.318 0 10.985 0 13h7v-1z"
-                      />
-                    </svg>
-                  </div>
-                  <h6 className="text-xl font-semibold">
-                    Leader sur le marché
-                  </h6>
-                  <p className="mt-2 mb-4 text-blueGray-500">
-                    Forts de notre expérience et de notre expertise, nous sommes
-                    le leader incontesté du secteur, avec des milliers de
-                    clients satisfaits.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="px-4 text-center">
-              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                <div className="px-4 py-5 flex-auto">
-                  <div className="text-white bg-primary p-3 text-center inline-flex items-center justify-center  mb-5 shadow-lg rounded-full">
-                    <svg
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      height="2em"
-                      width="2em"
-                    >
-                      <path d="M3 4h14v4h3l3 4v5h-2a3 3 0 01-3 3 3 3 0 01-3-3H9a3 3 0 01-3 3 3 3 0 01-3-3H1V6a2 2 0 012-2m14 5.5V12h4.47L19.5 9.5H17m-11 6A1.5 1.5 0 004.5 17 1.5 1.5 0 006 18.5 1.5 1.5 0 007.5 17 1.5 1.5 0 006 15.5m12 0a1.5 1.5 0 00-1.5 1.5 1.5 1.5 0 001.5 1.5 1.5 1.5 0 001.5-1.5 1.5 1.5 0 00-1.5-1.5M8 14l6-6-1.41-1.42L8 11.17 5.91 9.08 4.5 10.5 8 14z" />
-                    </svg>
-                  </div>
-                  <h6 className="text-xl font-semibold">Livraison rapide</h6>
-                  <p className="mt-2 mb-4 text-dark">
-                    Nous offrons une livraison express en moins de 24 heures,
-                    assurant que vos colis arrivent à temps, à chaque fois.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="px-4 text-center">
-              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                <div className="px-4 py-5 flex-auto">
-                  <div className="text-white bg-primary p-3 text-center inline-flex items-center justify-center  mb-5 shadow-lg rounded-full">
-                    <svg
-                      viewBox="0 0 16 16"
-                      fill="currentColor"
-                      height="2em"
-                      width="2em"
-                    >
-                      <path d="M3 4h14v4h3l3 4v5h-2a3 3 0 01-3 3 3 3 0 01-3-3H9a3 3 0 01-3 3 3 3 0 01-3-3H1V6a2 2 0 012-2m14 5.5V12h4.47L19.5 9.5H17m-11 6A1.5 1.5 0 004.5 17 1.5 1.5 0 006 18.5 1.5 1.5 0 007.5 17 1.5 1.5 0 006 15.5m12 0a1.5 1.5 0 00-1.5 1.5 1.5 1.5 0 001.5 1.5 1.5 1.5 0 001.5-1.5 1.5 1.5 0 00-1.5-1.5M8 14l6-6-1.41-1.42L8 11.17 5.91 9.08 4.5 10.5 8 14z" />
-                    </svg>
-                  </div>
-                  <h6 className="text-xl font-semibold">
-                    Satisfaction garantie
-                  </h6>
-                  <p className="mt-2 mb-4 text-dark">
-                    Notre engagement envers la qualité et la satisfaction client
-                    nous permet doffrir une garantie complète sur tous nos
-                    produits.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </section>
+    </>
   );
 }
-
-export default Carousel;
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,7 +1,6 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
-// import { CartContext } from "../contexts/CartContext";
 import SimilarProducts from "../components/SimilarProducts"
 import CartButton from "../components/AddToCartButton";
 export default function ProductPage() {
